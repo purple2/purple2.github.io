104c68a18bd3ca6f7ae7e51be8273f4051500e3c
purple2 <css491purple2@gmail.com>
2015-03-08 11:53:44 -0700

    Update music.js

--- music.js
+++ music.js
@@ -1,7 +1,7 @@
-function Music(game, filepath) {
+function Music(game, filepath, loop, volume) {
 	this.music = new Audio(filepath);
-	this.music.volume = 0.05;
-	this.music.loop = true;
+	this.music.volume = volume;
+	this.music.loop = loop;
 	this.unit = 0.008;
 }
 Music.prototype.play = function(){
