9962e7808368951fbf3023785236d137886ec1fe
purple2 <css491purple2@gmail.com>
2015-03-12 19:58:47 -0700

    Update boss.js

--- boss.js
+++ boss.js
@@ -486,14 +486,14 @@ Boss.prototype.update = function () {
 
 Boss.prototype.draw = function (ctx) {
     //commented this part out since is was used for setting hitboxes testing
-    ctx.fillStyle = "DarkGreen";
-    ctx.fillRect(this.myboxes.hitbox.x, this.myboxes.hitbox.y, this.myboxes.hitbox.width, this.myboxes.hitbox.height);
-    Entity.prototype.draw.call(this);
-    if (this.current_action) {
-        ctx.fillStyle = "Red";
-        ctx.fillRect(this.myboxes.attackbox.x, this.myboxes.attackbox.y, this.myboxes.attackbox.width, this.myboxes.attackbox.height);
-        Entity.prototype.draw.call(this);
-    }  
+    //ctx.fillStyle = "DarkGreen";
+    //ctx.fillRect(this.myboxes.hitbox.x, this.myboxes.hitbox.y, this.myboxes.hitbox.width, this.myboxes.hitbox.height);
+    //Entity.prototype.draw.call(this);
+    //if (this.current_action) {
+    //   ctx.fillStyle = "Red";
+    //    ctx.fillRect(this.myboxes.attackbox.x, this.myboxes.attackbox.y, this.myboxes.attackbox.width, this.myboxes.attackbox.height);
+    //    Entity.prototype.draw.call(this);
+    //}  
     if (this.jumping) {
         if (this.isRight) {
             this.boss_jumpAnimation.drawFrame(this.game, ctx, this.x, this.y - 340);
